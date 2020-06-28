08d699db689b91be8304cc05ef6ad5db33f9e792
Eyad Web Developmnet <30596418+development1soft@users.noreply.github.com>
2020-06-28 15:46:34 +0300

    Api Auth - Generate Token - Normal Auth Finished Successfully

---
+++ app/Listeners/GenerateToken.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Str;
+
+
+class GenerateToken
+{
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        $user = $event->user;
+        $user->api_token = Str::random(60);
+        $user->save();
+    }
+}
